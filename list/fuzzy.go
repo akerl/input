b0f52cba21e65db42dd5074f4ba829b4d1a9aa2c
Les Aker <me@lesaker.org>
2019-08-09 15:45:12 +0100

    add initial list layout

---
+++ list/fuzzy.go
@@ -0,0 +1,18 @@
+package list
+
+import (
+	"github.com/ktr0731/go-fuzzyfinder"
+)
+
+// FuzzyPrompt implements a fuzzy interactive picker
+type FuzzyPrompt struct{}
+
+// TODO: Support displaying message
+
+// Execute runs the list prompt
+func (f FuzzyPrompt) Execute(_ string, os OptionSet) (int, error) {
+	lines := make([]bool, len(os))
+	return fuzzyfinder.Find(lines, func(i int) string {
+		return os[i].String()
+	})
+}
---
+++ list/wmenu.go
@@ -0,0 +1,35 @@
+package list
+
+import (
+	"os"
+
+	"github.com/dixonwille/wmenu"
+)
+
+// WmenuPrompt implements a wmenu-based prompt
+type WmenuPrompt struct{}
+
+// Execute runs the list prompt
+func (w WmenuPrompt) Execute(msg string, optSet OptionSet) (int, error) {
+	c := make(chan int, 1)
+
+	menu := wmenu.NewMenu(msg)
+	menu.ChangeReaderWriter(os.Stdin, os.Stderr, os.Stderr)
+	menu.LoopOnInvalid()
+
+	actionFunc := func(opts []wmenu.Opt) error {
+		c <- opts[0].ID
+		return nil
+	}
+	menu.Action(actionFunc)
+
+	for _, option := range optSet {
+		menu.Option(option.String(), nil, false, nil)
+	}
+
+	if err := menu.Run(); err != nil {
+		return -1, err
+	}
+
+	return <-c, nil
+}
