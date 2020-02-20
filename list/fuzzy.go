d25fda4d047486c6d910c26bd063e92d20c133d2
Les Aker <me@lesaker.org>
2020-02-20 09:09:49 +0000

    add support for fuzzyfinder message

--- list/fuzzy.go
+++ list/fuzzy.go
@@ -7,13 +7,18 @@ import (
 // FuzzyPrompt implements a fuzzy interactive picker
 type FuzzyPrompt struct{}
 
-// TODO: Support displaying message
-
 // Execute runs the list prompt
-func (f FuzzyPrompt) Execute(_ string, os OptionSet) (int, error) {
+func (f FuzzyPrompt) Execute(msg string, os OptionSet) (int, error) {
 	logger.InfoMsg("executing fuzzy prompt")
 	lines := make([]bool, len(os))
-	return fuzzyfinder.Find(lines, func(i int) string {
+
+	handler = func(i int) string {
 		return os[i].String()
-	})
+	}
+
+	return fuzzyfinder.Find(
+		lines,
+		handler,
+		fuzzyfiner.WithPromptString(msg),
+	)
 }
